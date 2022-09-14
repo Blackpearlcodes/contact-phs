7f286e51064152f9cbdb4aa1cc6c5aabf756ac45
Ghassan Fares <gfares@gmail.com>
2022-09-14 11:58:20 +0300

    php edit 1

---
+++ contact.php
@@ -0,0 +1,8 @@
+<?php
+// database connection code
+if(isset($_POST['txtName']))
+{
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+$con = mysqli_connect('localhost', 'root', '','db_contact');
+
+?>
